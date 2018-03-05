c11c297416c3ee0874e330428cad229710dfb330
a.munoz <anpaumuo@inf.upv.es>
2018-03-05 23:46:20 +0100

    Commit to check if that works

--- scritp.js
+++ scritp.js
@@ -1,7 +1,7 @@
 var TelegramBot = require('node-telegram-bot-api');
 
 var token = '518691232:AAGa8l0fTzvMNHcUhMdLc7rznPrj6JQRZsc';
-// Setup polling way
+// Setup polling way for the bot
 var bot = new TelegramBot(token, {polling: true});
 bot.on('text', function (msg) {
   var chatId = msg.chat.id;
