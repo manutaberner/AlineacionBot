5c79e074e4d932249693764da4679e4a230174dc
LordHub <anpaumuo@inf.upv.es>
2018-03-05 23:28:13 +0100

    Add files via upload

---
+++ scritp.js
@@ -0,0 +1,9 @@
+var TelegramBot = require('node-telegram-bot-api');
+
+var token = '518691232:AAGa8l0fTzvMNHcUhMdLc7rznPrj6JQRZsc';
+// Setup polling way
+var bot = new TelegramBot(token, {polling: true});
+bot.on('text', function (msg) {
+  var chatId = msg.chat.id;
+  bot.sendMessage(chatId,"Hola Mundo");
+});
