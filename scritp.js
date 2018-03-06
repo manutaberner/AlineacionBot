f43cc2b871035a201b8d96204a1e0399977a13d0
a.munoz <anpaumuo@inf.upv.es>
2018-03-06 01:02:10 +0100

    Working on the Bot

--- scritp.js
+++ scritp.js
@@ -1,9 +1,45 @@
-var TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api'); 
+const token = '518691232:AAGa8l0fTzvMNHcUhMdLc7rznPrj6JQRZsc';
+const bot = new TelegramBot(token, {polling: true});
+    
+// bot.on('message', (msg) => {
+//   //anything
+//   var chatId = msg.chat.id;
+//   bot.sendMessage(chatId,"Hola Mundito");
+// });
 
-var token = '518691232:AAGa8l0fTzvMNHcUhMdLc7rznPrj6JQRZsc';
-// Setup polling way for the bot
-var bot = new TelegramBot(token, {polling: true});
-bot.on('text', function (msg) {
-  var chatId = msg.chat.id;
-  bot.sendMessage(chatId,"Hola Mundo");
-});
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Creation", {
+  "reply_markup": {
+      "keyboard": [["Crear Alineacion"],["Mis Alineaciones"], ["Bye!"]]
+      }
+  });
+      
+  });
+
+  // Define functionality according to the start commands
+  // bot.on('message', (msg) => {
+  //   var create = "Create Alineacion";
+  //   if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+  //       bot.sendMessage(msg.chat.id, "/setdefenders");
+  //   }
+  //   var bye = "Bye!";
+  //   if (msg.text.toString().toLowerCase().includes(bye)) {
+  //       bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
+  //   }    
+  //   var robot = "My Alineaciones";
+  //   if (msg.text.indexOf(robot) === 0) {
+  //       bot.sendMessage(msg.chat.id, "Yes I'm robot but not in that way!");
+  //   }
+  //});
+
+
+    //Set Defenders
+    bot.onText(/\/setdefenders/, (msg) => {
+      bot.sendMessage(msg.chat.id, "setdefenders", {
+      "reply_markup": {
+          "keyboard": [["3 Defensas"], ["4 Defensas"], ["5 Defensas"]]
+          }
+      });
+          
+      });
