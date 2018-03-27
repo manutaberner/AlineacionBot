0d5e2c995175b2b8d9a4287cbd586171396e331c
a.munoz <anpaumuo@inf.upv.es>
2018-03-27 17:38:24 +0200

    Workflow completed. Now returns the lineup.

--- script3.js
+++ script3.js
@@ -2,55 +2,121 @@
 const TelegramBot = require('node-telegram-bot-api'); 
 const token = '518691232:AAGa8l0fTzvMNHcUhMdLc7rznPrj6JQRZsc';
 const bot = new TelegramBot(token, {polling: true});
+var alineacion = []; 
+//Include the goolkepeer
+alineacion.push(1);
+var messagesToSend = ["/setmidfielders","/setattackers"];
+var lastMessage;
+bot.onText(/\/start/, (msg) => {
+    
+    bot.sendMessage(msg.chat.id, "Welcome", {
+    "reply_markup": {
+        "keyboard": [["/setdefenders", "Second sample"],   ["Keyboard"], ["I'm robot"]],
+        "resize_keyboard" : true,
+        "one_time_keyboard" : true
+        }
+    });        
+});
+    
+       //Set Defenders
+bot.onText(/\/setdefenders/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Indicar el numero de Defensas deseado:", {
+        "reply_markup": {
+            "keyboard": [["3 Defensas"],["4 Defensas"], ["5 Defensas"]],
+            "resize_keyboard" : true,
+            "one_time_keyboard" : true
+        }
+    });
+});
+
+  //Set Midfielders
+  bot.onText(/\/setmidfielders/, (msg) => {  
+    bot.sendMessage(msg.chat.id, "Indicar el numero de Centrocampistas deseado:", {
+        "reply_markup": {
+            "keyboard": [
+                ["1 Centrocampista"],
+                ["2 Centrocampistas"],
+                ["3 Centrocampistas"],
+                ["4 Centrocampistas"],
+                ["5 Centrocampistas"]],
+            "resize_keyboard" : true,
+            "one_time_keyboard" : true
+        }
+    });
+});
+
+//Set Attackers - Not useful anymore
+bot.onText(/\/setattackers/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Indicar el numero de Delanteros deseado:", {
+        "reply_markup": {
+            "keyboard": [["1 Delanteros"],["2 Delanteros"], ["3 Delanteros"]],
+            "resize_keyboard" : true,
+            "one_time_keyboard" : true
+        }
+    });
+    getLastMessage(msg);
+    console.log(alineacion);
+});
+//If the message has the word "Defensas" = Defenders
 bot.on('message', (msg) => {
-
-    var hi = "hi";
-    if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
-    bot.sendMessage(msg.chat.id,"Hello dear user");
+    
+    var defenders = "defensas";
+    var midfielders = "centrocampista";
+    if (msg.text.toString().toLowerCase().includes(defenders)) {
+        getLastMessage(msg);
+        bot.sendMessage(msg.chat.id,"Haga click en el mensaje  para continuar");
+        bot.sendMessage(msg.chat.id,"/setmidfielders"); 
     } 
+    
+    if (msg.text.toString().toLowerCase().includes(midfielders)) {
+        getLastMessage(msg);
+        fillLineUp(msg);
+        //bot.sendMessage(msg.chat.id,"Haga click en el mensaje para continuar");
+        //bot.sendMessage(msg.chat.id,"/setattackers"); 
         
-    var bye = "bye";
-    if (msg.text.toString().toLowerCase().includes(bye)) {
-    bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
-    } 
-    
-    });
-    bot.onText(/\/start/, (msg) => {
-    
-        bot.sendMessage(msg.chat.id, "Welcome", {
-        "reply_markup": {
-            "keyboard": [["/start2", "Second sample"],   ["Keyboard"], ["I'm robot"]],
-            'resize_keyboard' : true,
-            "one_time_keyboard" : true
-            }
-        });
-            
-    });
-    
-
-    bot.onText(/\/start2/, (msg) => {
-    
-        bot.sendMessage(msg.chat.id, "Welcome2", {
-        "reply_markup": {
-            "keyboard": [["/start3", "Second sample"],   ["Keyboard"], ["I'm robot"]],
-            'resize_keyboard' : true,
-            "one_time_keyboard" : true
-            }
-        });
-            
+        } 
     });
 
-    bot.onText(/\/start3/, (msg) => {
-        // JSON object that contains custom reply markup
-        var keyboard = {
-            reply_markup: JSON.stringify({
-              keyboard: [
-                ['Crear Alineacion'],
-                ['Mis Alineaciones'],
-                ['Bye!']
-              ]
-            })
-        };
-       //send msg to the chat
-        bot.sendMessage(msg.chat.id, text, keyboard);
-    });
+function sendNextMessage(msg)
+{
+    //Return the 1st message that has to be send to make the lineup
+    bot.sendMessage(msg.chat.id,messagesToSend[0]);
+    //remove the element already sent
+    messagesToSend.unshift();
+
+}
+
+function getLastMessage(msg){
+    var fullText = msg.text;
+    //get the number of the to add to the Array
+    lastMessage = parseInt(fullText.substring(0,1),10);
+    alineacion.push(lastMessage);
+}
+
+function fillLineUp(msg)
+{
+    var total = 11;
+    var lineUpWithLines="Alineacion: ";
+    for(var i = 0; i < alineacion.length ; i ++)
+    {
+        lineUpWithLines += alineacion[i] + "-";
+        total -= alineacion[i];
+    }
+    lineUpWithLines += total;
+    bot.sendMessage(msg.chat.id,lineUpWithLines);
+    alineacion.push(total);
+}
+
+
+//Useless
+function getLineUp(msg)
+{
+    bot.sendMessage(msg.chat.id,alineacion);
+    for(var i = 0; i < alineacion.length ; i ++)
+    {
+        for( var j = 0 ; j <alineacion[i] ; j++)
+        {
+
+        }
+    }
+}
