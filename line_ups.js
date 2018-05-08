8642bddd678f44af997a4249036422c8925c31f5
a.munoz <anpaumuo@inf.upv.es>
2018-05-08 19:07:36 +0200

    Corrected error when pushing to Array. Need to create var like in Bm

--- line_ups.js
+++ line_ups.js
@@ -5,67 +5,99 @@ var array3Midfilders =[[327,296,327],[109,222,340]];
 var array3Attackers = [[450,503,450],[90,222,345]];
 var midfildersArray = {p1:{x1 : 327, y1: 109}, p2:{ x2: 296 , y2 : 222}, p3:{x3 : 327, y3 : 340} };
 var attackersArray = {p1:{x1 : 450, y1: 90}, p2:{ x2: 503 , y2 : 222}, p3 :{ x3 : 450, y3 : 345} };
-var fullXArray; //Array storing the X coordinates
-var fullYArray; //Array storing the Y coordinates
+var fullXArray = []; //Array storing the X coordinates
+var fullYArray = []; //Array storing the Y coordinates
 
-function addCoordinatesDefenders(numberOfDefenders) 
-{
-    //Add the goalkeepers Coordinates
-    fullXArray.push(goalkeeperPoint[0]);
-    fullYArray.push(goalkeeperPoint[1]);
-    switch (numberOfDefenders) {
-        case 4:
-        for (let i = 0 ; i < numberOfDefenders ; i++)
-        {
-            fullXArray.push(array4Defenders[0][i]);
-            fullXArray.push(array4Defenders[1][i]);
+// module.exports = function() { 
+//     this.addCoordinatesDefenders = function (numberOfDefenders) 
+//     {
+//         //Empty the array just in case
+//         emptyArray(fullXArray); emptyArray(fullYArray);
+//         //Add the goalkeepers        
+//         fullXArray.push(goalkeeperPoint[0]);
+//         fullYArray.push(goalkeeperPoint[1]);
+//         switch (numberOfDefenders) {
+//             case 4:
+//             for (let i = 0 ; i < numberOfDefenders ; i++)
+//             {
+//                 fullXArray.push(array4Defenders[0][i]);
+//                 console.log("Adding position"+array4Defenders[0][i]+"-"+array4Defenders[1][i])
+//                 fullYArray.push(array4Defenders[1][i]);
+//             }
+//                 break;
+        
+//             default:
+//                 break;
+//         }
+//     };
+
+//     this.addCoordinatesMidfilders = function (numberOfMidfilders) 
+//     {
+//         //Switch for the defenders
+//         switch (numberOfMidfilders) {
+//             case 3:
+//                 for (let i = 0 ; i < numberOfMidfilders ; i++)
+//                 {
+//                     fullXArray.push(array3Midfilders[0][i]);
+//                     fullYArray.push(array3Midfilders[1][i]);
+//                 }
+//                 break;
+        
+//             default: 
+//                 break;
+//         }
+//     };
+//     this.addCoordinatesAttackers = function (numberOfAttackers) 
+//     {
+//         //Switch for the defenders
+//         switch (numberOfAttackers) {
+//             case 3:
+//                 for (let i = 0 ; i < numberOfAttackers ; i++)
+//                 {
+//                     fullXArray.push(array3Attackers[0][i]);
+//                     fullYArray.push(array3Attackers[1][i]);
+//                 }
+//                 break;
+        
+//             default:
+//                 break;
+//         }
+//     };
+//     this.getFullXArray = function() {return fullXArray;};
+//     this.getFullYArray = function() {return fullYArray;};
+// }
+
+// function emptyArray(arrayToEmpty)
+// {
+//     arrayToEmpty = [];
+// }
+
+var methods = {};
+
+methods.timestamp = function() {
+	console.log('Current Time in Unix Timestamp: ' + Math.floor(Date.now() / 1000))
+};
+methods.currentDate = function() {
+	console.log('Current Date is: ' + new Date().toISOString().slice(0, 10))
+};
+
+methods.test =function(numberOfAttackers){
+            //Switch for the defenders
+        switch (numberOfAttackers) {
+            case 3:
+                for (let i = 0 ; i < numberOfAttackers ; i++)
+                {
+                    fullXArray.push(array3Attackers[0][i]);
+                    fullYArray.push(array3Attackers[1][i]);
+                }
+                console.log(fullXArray);
+                console.log(fullYArray);
+                break;
+        
+            default:
+                break;
         }
-            break;
-    
-        default:
-            break;
-    }
-}
+        
+};
 
-function addCoordinatesMidfilders(numberOfMidfilders) 
-{
-    //Switch for the defenders
-    switch (numberOfMidfilders) {
-        case 3:
-            for (let i = 0 ; i < numberOfMidfilders ; i++)
-            {
-                fullXArray.push(array3Midfilders[0][i]);
-                fullXArray.push(array3Midfilders[1][i]);
-            }
-            break;
-    
-        default:
-            break;
-    }
-}
-
-
-function addCoordinatesAttackers(numberOfAttackers) 
-{
-    //Switch for the defenders
-    switch (numberOfAttackers) {
-        case 3:
-            for (let i = 0 ; i < numberOfAttackers ; i++)
-            {
-                fullXArray.push(array3Attackers[0][i]);
-                fullXArray.push(array3Attackers[1][i]);
-            }
-            break;
-    
-        default:
-            break;
-    }
-}
-
-getFullXArray = function(){
-    return fullXArray;
-}
-
-getFullYArray = function(){
-    return fullYArray;
-}
+exports.data = methods;
