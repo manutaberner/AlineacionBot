e33e395398d2db2ee39b5edaf6f1f90125c6abac
Andres Paul Muñoz <andrespaul.dtt@gmail.com>
2019-08-25 10:57:18 +0200

    Add old changes + images

--- final.js
+++ final.js
@@ -14,6 +14,7 @@ var storeNames = false;
 var goalkeeperImage = "Images/goalkeeper.jpg"; 
 var goalkeeperImagetoPrint = goalkeeperImage;
 var loadedImage;
+var imageLoaded ;
 var imagesToPrint = ["Images/goalkeeper.jpg","Images/4_defenders.png","Images/3_midfilders.png","Images/3_attackers.png"];
 var midfildersImages = ["Images/3_midfilders.png"];
 var createImage = [];
@@ -32,7 +33,7 @@ bot.onText(/\/start/, (msg) => {
    alineacionWithNames = []; counter = 11; 
     bot.sendMessage(msg.chat.id, "Welcome to the football line up creator!", {
     "reply_markup": {
-        "keyboard": [["/getplayers"],["/4_3_3"],["/printlineup"]],
+        "keyboard": [["/getplayers"],["/4_3_3"],["/printlineup"],["/test"]],
         "resize_keyboard" : true,
         "one_time_keyboard" : true
         }
@@ -63,6 +64,13 @@ bot.onText(/\/printlineup/, (msg) => {
 bot.onText(/\/getplayers/, (msg) => {  
     bot.sendMessage(msg.chat.id, "Por favor introduzca el nombre de los futbolistas (1 nombre por mensaje)");
     storeNames = true;
+});
+
+
+bot.onText(/\/test/, (msg) => {  
+    bot.sendMessage(msg.chat.id, "Por favor introduzca el nombre de los futbolistas (1 nombre por mensaje)");
+    printGoalkeeperNameOnImage("juan", 75,222);
+
 });
 
 //Gets the name of the players
@@ -103,19 +111,23 @@ function getLastMessage(msg){
 
 
 //Jimp fuction that prints the name of the goalkeeper
-function printGoalkeeperNameOnImage(testName){
-Jimp.read(goalkeeperImagetoPrint)
-    .then(function (image) {
-        loadedImage = image;
-        return Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
-    })
-    .then(function (font) {
-        loadedImage.print(font, 75, 222, testName)
-                   .write(goalkeeperImagetoPrint);
-    })
-    .catch(function (err) {
-        console.error(err);
-    });
+function printGoalkeeperNameOnImage(testName,x,y){
+    for (let index = 0; index < 120; index+=40) { 
+
+        Jimp.read(goalkeeperImagetoPrint)
+        .then(function (image) {
+            loadedImage = image;
+            return Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
+        })
+        .then(function (font) {
+            loadedImage.print(font, x + index, y, testName)
+                       .write(goalkeeperImagetoPrint);
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+    }
+
     
 }
 
@@ -127,29 +139,42 @@ function joinImages()
         data[0].composite(data[1],0,0);
         data[0].composite(data[2],0,0);
         data[0].composite(data[3],0,0);
-        data[0].write("Images/fullImage.png");
-        loadedImage = "Images/fullImage.png";
+        data[0].write("Images/fullImage.jpg");
+        imageLoaded = "Images/fullImage.jpg";
     });
 }
 
 function printNamesOnImage( fullXArray , fullYArray , msg){
 
+    console.log(fullXArray);
+    console.log(fullYArray);
+    console.log(alineacionWithNames);
      for (let index = 0; index < fullXArray.length; index++) 
     {
-        Jimp.read(loadedImage)
+        let cordX = fullXArray[index];
+        console.log("Coordinate X:");
+        console.log(cordX);
+        let cordY = fullYArray[index];
+        console.log("Coordinate Y:");
+        console.log(cordY);
+        let nameToPrint = alineacionWithNames[index].toString();
+        console.log("Name to print:");
+        console.log(nameToPrint);
+
+        Jimp.read(imageLoaded)
         .then(function (image) {
             loadedImage = image;
             return Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
         })
         .then(function (font) {
-            loadedImage.print(font, fullXArray[index], fullYArray[index], alineacionWithNames[index].toString())
+            loadedImage.print(font, cordX, cordY, nameToPrint)
                        .write(loadedImage);
         })
         .catch(function (err) {
             console.error(err);
         });       
     }   
-    bot.sendPhoto(msg.chat.id, loadedImage);
+    bot.sendPhoto(msg.chat.id, imageLoaded);
 }
 
 //Store lineup chosen into an Array
