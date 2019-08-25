baed3a5db295f100bff0a255721b6acca285d032
Unknown <anpaumuo@inf.upv.es>
2019-08-25 12:54:09 +0200

    refactor + wip

--- final.js
+++ final.js
@@ -1,11 +1,11 @@
 'use strict'
-const TelegramBot = require('node-telegram-bot-api'); 
-const token = '518691232:AAGa8l0fTzvMNHcUhMdLc7rznPrj6JQRZsc';
-const bot = new TelegramBot(token, {polling: true});
-//library to manage images
+const TelegramBot = require('node-telegram-bot-api');
+const config = require('./config/config.json');
+const bot = new TelegramBot(config.token, {polling: true});
+// Library to manage images
 var Jimp = require("jimp");
 var tools = require('./line_ups.js');
-//library to manage asynchronity
+// Asynchronous calls
 var Waterfall = require('async-waterfall');
 var picLink = "http://images.all-free-download.com/images/graphiclarge/soccer_field_311115.jpg";
 var alineacionWithNames = [];
@@ -13,17 +13,19 @@ var counter; //counter for the 11 players
 var storeNames = false; 
 var goalkeeperImage = "Images/goalkeeper.jpg"; 
 var goalkeeperImagetoPrint = goalkeeperImage;
-var loadedImage;
-var imageLoaded ;
-var imagesToPrint = ["Images/goalkeeper.jpg","Images/4_defenders.png","Images/3_midfilders.png","Images/3_attackers.png"];
-var midfildersImages = ["Images/3_midfilders.png"];
+let loadedImage;
+let imageLoaded ;
+const imagesToPrint = ["Images/goalkeeper.jpg","Images/4_defenders.png","Images/3_midfilders.png","Images/3_attackers.png"];
+const midfildersImages = ["Images/3_midfilders.png"];
 var createImage = [];
 var alineacionInMessage;
-var defendersNumber; var midfildersNumber; var attackersNumber;
+var defendersNumber;
+var midfildersNumber;
+var attackersNumber;
 
 
 //get all the images into the array
-for(var i=0; i< imagesToPrint.length; i++)
+for(let i=0; i< imagesToPrint.length; i++)
 {
     createImage.push(Jimp.read(imagesToPrint[i]));
 }
@@ -42,22 +44,37 @@ bot.onText(/\/start/, (msg) => {
 
 });
 
- bot.onText(/\/4_3_3/, (msg) => {  
-    bot.sendMessage(msg.chat.id,"Por favor introduzca los nombres en el orden indicado en la imagen")
-    bot.sendPhoto(msg.chat.id,"Images/"+getLastMessage(msg).substring(1)+"_numbers.png");
-    alineacionInMessage = getLastMessage(msg).substring(1);
-    bot.sendMessage(msg.chat.id,"Haga click en el mensaje  para continuar");
-    bot.sendMessage(msg.chat.id,"/getplayers"); 
-    joinImages(getLastMessage(msg).substring(1));
-    console.log("alineacion: "+ alineacionInMessage);
-    msgToVariables(alineacionInMessage);
-    
+ bot.onText(/\/4_3_3/, (msg) => {
+    let chatId = msg.chat.id
+    bot.sendMessage(chatId,"Por favor introduzca los nombres en el orden indicado en la imagen")
+        .then(() => {
+            return bot.sendPhoto(chatId,"Images/"+getLastMessage(msg).substring(1)+"_numbers.png")
+        })
+        .then(() => {
+            alineacionInMessage = getLastMessage(msg).substring(1);
+            return bot.sendMessage(chatId,"Haga click en el mensaje  para continuar");
+        })
+        .then(() => {
+            return bot.sendMessage(chatId,"/getplayers");
+        })
+        .then(() => {
+            joinImages(getLastMessage(msg).substring(1));
+            console.log("alineacion: "+ alineacionInMessage);
+            msgToVariables(alineacionInMessage);
+        })
+        .catch((err) => {
+            console.error(err)
+        });
 });
 
 bot.onText(/\/printlineup/, (msg) => {
+    let chatId = msg.chat.id
     for (let index = 0; index < alineacionWithNames.length; index++) 
     {
-        bot.sendMessage(msg.chat.id,alineacionWithNames[index]);
+        bot.sendMessage(chatId, alineacionWithNames[index])
+            .catch((err) => {
+                console.error(err)
+            })
     }
 });
 
@@ -70,12 +87,12 @@ bot.onText(/\/getplayers/, (msg) => {
 bot.onText(/\/test/, (msg) => {  
     bot.sendMessage(msg.chat.id, "Por favor introduzca el nombre de los futbolistas (1 nombre por mensaje)");
     printGoalkeeperNameOnImage("juan", 75,222);
-
 });
 
 //Gets the name of the players
 bot.on('message', (msg) => {
-    var notCommand = "/";
+    const notCommand = "/";
+    const chatId = msg.chat.id;
     if (msg.text.toString().toLowerCase().includes(notCommand) && !storeNames) 
         {
             console.log("It's a command");
@@ -88,29 +105,32 @@ bot.on('message', (msg) => {
         {
             alineacionWithNames.push(getLastMessage(msg));
             storeNames = false;
-            bot.sendMessage(msg.chat.id, "Alineacion completa!");
-            tools.data.addCoordinatesDefenders(defendersNumber);
-            tools.data.addCoordinatesMidfilders(midfildersNumber);
-            tools.data.addCoordinatesAttackers(attackersNumber);
-            counter --;
-            let aux1 = tools.data.getFullXArray();
-            let aux2 = tools.data.getFullYArray();
-            printNamesOnImage(aux1,aux2,msg);
-
+            bot.sendMessage(chatId, "Alineacion completa!")
+                .then(() => {
+                    tools.data.addCoordinatesDefenders(defendersNumber);
+                    tools.data.addCoordinatesMidfilders(midfildersNumber);
+                    tools.data.addCoordinatesAttackers(attackersNumber);
+                    counter --;
+                    let aux1 = tools.data.getFullXArray();
+                    let aux2 = tools.data.getFullYArray();
+                    printNamesOnImage(aux1,aux2,msg);
+                })
+                .catch((err) => {
+                    console.error(err)
+                })
         }
         else if (counter === 0)
         {
-            bot.sendMessage(msg.chat.id, "Alineacion completa!"); 
+            bot.sendMessage(chatId, "Alineacion completa!");
         }
 });
 
 function getLastMessage(msg){
-    var fullText = msg.text;
-    return fullText;
+    return msg.text;
 }
 
 
-//Jimp fuction that prints the name of the goalkeeper
+// Jimp function that prints the name of the goalkeeper
 function printGoalkeeperNameOnImage(testName,x,y){
     for (let index = 0; index < 120; index+=40) { 
 
@@ -146,20 +166,14 @@ function joinImages()
 
 function printNamesOnImage( fullXArray , fullYArray , msg){
 
-    console.log(fullXArray);
-    console.log(fullYArray);
-    console.log(alineacionWithNames);
-     for (let index = 0; index < fullXArray.length; index++) 
+     for (let index = 0; index < fullXArray.length; index++)
     {
         let cordX = fullXArray[index];
-        console.log("Coordinate X:");
-        console.log(cordX);
+        console.log("Coordinate X:", cordX);
         let cordY = fullYArray[index];
-        console.log("Coordinate Y:");
-        console.log(cordY);
+        console.log("Coordinate Y:", cordY);
         let nameToPrint = alineacionWithNames[index].toString();
-        console.log("Name to print:");
-        console.log(nameToPrint);
+        console.log("Name to print:", nameToPrint);
 
         Jimp.read(imageLoaded)
         .then(function (image) {
@@ -168,12 +182,12 @@ function printNamesOnImage( fullXArray , fullYArray , msg){
         })
         .then(function (font) {
             loadedImage.print(font, cordX, cordY, nameToPrint)
-                       .write(loadedImage);
+                       .write("Images/fullImage.jpg");
         })
         .catch(function (err) {
             console.error(err);
-        });       
-    }   
+        });
+    }
     bot.sendPhoto(msg.chat.id, imageLoaded);
 }
 
