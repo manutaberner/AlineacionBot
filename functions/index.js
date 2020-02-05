d80d450e34f6898c986466d3c2ab7a16add01824
Unknown <anpaumuo@inf.upv.es>
2020-02-05 18:51:36 +0100

    Added firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,69 @@
 const Telegraf = require('telegraf');
 const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 
-const bot = new Telegraf('518691232:AAGa8l0fTzvMNHcUhMdLc7rznPrj6JQRZsc');
+const bot = new Telegraf(''); // TODO insert telegram bot token
 
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.launch();
+const db = admin.database();
+const ref = db.ref("/");
+const bucket = admin.storage().bucket();
+
+bot.start((ctx) => {
+    const chatId = ctx.chat.id
+
+    return ref.child(`users/${chatId}`).once("value", snapshot => {
+        if (snapshot.exists()) {
+            const userData = snapshot.val();
+            return ctx.reply('Hi ' + userData.username);
+        } else {
+            ref.child("users/" + chatId).set({
+                counter: 0,
+                created_at: Date.now(),
+                favourite_line_up: '',
+                username: ctx.message.from.username
+            });
+            return ctx.reply('Bienvenido a  Alineacion Bot');
+        }
+    }).catch((error) => {
+        console.error(error);
+        return false;
+    });
+});
+
+bot.command('4_3_3', (ctx) => {
+
+});
+// ctx.telegram.sendPhoto(, {source: '../../assets/img/db_20180503_095807_867.jpg'})
+
+bot.launch()
+    .catch((error) => {
+        console.error(error);
+        return false;
+    });
+
 
 exports.bot = functions.https.onRequest((req, res) => {
-    return bot.handleUpdate(req.body, res);
+    return ref.child("testdata")
+        .set({
+            alanisawesome: {
+                date_of_birth: "June 234, 1912",
+                full_name: "Alan Turing"
+            },
+            gracehop: {
+                date_of_birth: "December 93, 1906",
+                full_name: "Grace Hopper"
+            }
+        }).then((result) => {
+            console.info(result);
+            return result;
+        }).catch((error) => {
+            console.error(error);
+            return false;
+        });
     // return res.send("Hello from Alineacion bot!");
-})
+});
+
+function getProfile(chatId) {
+    return ref.child("users/" + chatId).get();
+}
