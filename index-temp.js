59f54135c5ef1c8c997e2d4c86bf5ce011aae321
magnumelio <manutabernerllorca@gmail.com>
2020-02-13 20:04:51 +0100

    Espacio

--- index-temp.js
+++ index-temp.js
@@ -6,6 +6,7 @@
 // bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 // bot.launch();
 //
+//
 // exports.bot = functions.https.onRequest((req, res) => {
 //     return bot.handleUpdate(req.body, res);
 //     // return res.send("Hello from Alineacion bot!");
